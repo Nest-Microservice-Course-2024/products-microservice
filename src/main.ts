834635029e91715b4c7ffdf7e08378666b09339a
Efraín González <efgonzalezca@gmail.com>
2024-07-16 23:03:10 -0500

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+import { envs } from './config/envs';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const logger = new Logger('Main');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    }
+  )
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+  await app.listen();
+  logger.log(`Products microservice running on http://localhost:${envs.port}`);
+}
+bootstrap();
